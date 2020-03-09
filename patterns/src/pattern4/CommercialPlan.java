f4f4040023d715b5311988259bdff0027509670d
akhilareddy-12 <chittireddyakhila20@gmail.com>
2020-03-09 19:23:40 +0530

    done

---
+++ patterns/src/pattern4/CommercialPlan.java
@@ -0,0 +1,9 @@
+package pattern4;
+
+	class  CommercialPlan extends Plan{  
+		   //@override   
+		    public void getRate(){   
+		        rate=7.50;  
+		   }   
+	}//end of CommercialPlan class.  
+
---
+++ patterns/src/pattern4/DomesticPlan.java
@@ -0,0 +1,9 @@
+package pattern4;
+
+	class  DomesticPlan extends Plan{  
+        //@override  
+         public void getRate(){  
+             rate=3.50;              
+        }  
+   }//end of DomesticPlan class.  
+
